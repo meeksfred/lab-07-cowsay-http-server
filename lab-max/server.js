3fba8140a5f5f445904d1581b521f29028b06ad4
meeksfred <maxfred0025@gmail.com>
2016-09-22 08:55:35 -0700

    updated for pr comments

--- lab-max/server.js
+++ lab-max/server.js
@@ -14,41 +14,43 @@ const server = http.createServer(function(req, res){
   req.url.query = queryString.parse(req.url.query);
 
   if (req.method === 'GET' && req.url.pathname === '/'){
-    res.writeHead('200', {'Content-Type': 'text/plain'});
+    res.writeHead(200, {
+      'Content-Type': 'text/plain',
+    });
     res.write('hello world\n');
     res.end();
   }
 
   if (req.method === 'GET' && req.url.pathname === '/cowsay'){
     if (req.url.query.text){
-      res.writeHead('200', {'Content-Type': 'text/plain'});
+      res.writeHead(200, {
+        'Content-Type': 'text/plain',
+      });
       res.write(cowsay.say({text: req.url.query.text, f: req.url.query.f}));
       res.end();
-    } else {
-      res.writeHead('400', {'Content-Type': 'text/plain'});
-      res.write(cowsay.say({text: 'bad request\n try: localhost:3000/cowsay?text=howdy'}));
-      res.end();
     }
+    res.writeHead(400, {
+      'Content-Type': 'text/plain',
+    });
+    res.write(cowsay.say({text: 'bad request\n try: localhost:3000/cowsay?text=howdy'}));
+    res.end();
   }
 
   if(req.method === 'POST' && req.url.pathname === '/cowsay'){
     parseBody(req, function(err){
-      if (err) {
-        res.writeHead('400', {'Content-Type': 'text/plain'});
-        res.write(cowsay.say({text: 'bad request\ntry: localhost:3000/cowsay?text=howdy'}));
-        console.log('There is an error', err);
-        res.end();
-      }
-      else if(req.body.text){
-        res.writeHead('200', {'Content-Type': 'text/plain'});
+      if (err) return console.log('There is an error', err);
+      if (req.body.text) {
+        res.writeHead(200, {
+          'Content-Type': 'text/plain',
+        });
         res.write(cowsay.say({text: req.body.text, f: req.url.query.f}));
         res.end();
       }
-      else if(!req.body.text) {
-        res.writeHead('400', {'Content-Type': 'text/plain'});
-        res.write(cowsay.say({text: 'bad request\ntry: localhost:3000/cowsay?text=howdy'}));
-        res.end();
-      }
+      res.writeHead(400, {
+        'Content-Type': 'text/plain',
+      });
+      res.write(cowsay.say({text: 'bad request\ntry: localhost:3000/cowsay?text=howdy'}));
+      res.end();
     });
   }
 });
